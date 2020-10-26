8c75acd05410d67215e1bf79e11e68bc1b47e32a
hriday86691 <73236995+hriday86691@users.noreply.github.com>
2020-10-26 18:21:57 +0530

    Add files via upload

---
+++ simple dictionary.py
@@ -0,0 +1,11 @@
+#this simple Dictionary
+a={"Man":"মানুষ","Hand":"হাত","Leg":"পা","Hair":"চুল","Nose":"নাক","Finger":"আঙ্গুল","Knee":"হাঁটু" ,"Eye":"চোখ","Teeth":"দাঁত ","Skin":"চামড়া"}
+#user_input
+b=input("enter your word\n")
+#For_capital and smaller
+c=b.capitalize()
+#if_function
+if c in a:
+	print(c,"mean", a[c])
+else:
+	print("not available")
